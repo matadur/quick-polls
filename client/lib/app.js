f0b6d1977587209d7e8cf7303600236fa6915a22
Matt Durr <matt@onedurr.com>
2016-08-29 17:22:23 -0500

    take poll page template layout, added and styled buttons

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,15 +1,3 @@
-Template.takePollPage.helpers({
-
-  polls: function(){
-    return Polls.find()
-  },
-  nextPoll: function() {
-    var items = Polls.find({}, {sort: {_id : 1} }).fetch();
-    return items.slice(-1);
-  }
-
-});
-
 UI.registerHelper('indexedArray', function(context, options) {
   if (context) {
     return context.map(function(item, index) {
