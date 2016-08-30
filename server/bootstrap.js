6b40aa5f6b65630bb53126ba7d3a748f9c15b703
Matt Durr <matt@onedurr.com>
2016-08-30 10:42:17 -0500

    add remaining seed polls

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,19 +1,12 @@
 Meteor.startup(function() {
 
+  // Polls.remove({})
+
   // if there are no polls available
   if (Polls.find().count() === 0) {
 
     // create sample polls
-    var samplePolls = [{
-      question: 'The Taj Mahal is located where in India?',
-      choices:[{text:'Chennai'},{text:'Mumbai'},{text:'Agra'},{text:'Other'}],
-      infoUrl:'http://www.tajmahal.gov.in/',
-      submitter:'Matt Durr'
-    },{
-      question: 'What name was given to Superman by his birth parents?',
-      choices:[{text:'Clark Joseph Kent'},{text:'Kal-El'},{text:'Bruce Wayne'},{text:'Jor-El'}],
-      infoUrl:'https://www.google.com/?ion=1&espv=2#q=superman%27s%20origin'
-    }]
+    var samplePolls = getSeedPolls()
 
     // loop over each sample poll and insert into database
     _.each(samplePolls, function(poll) {
---
+++ server/seedPolls.js
@@ -0,0 +1,55 @@
+ getSeedPolls = function(){
+  return [
+  {
+    question: 'What name was given to Superman by his birth parents?',
+    choices:[{text:'Clark Joseph Kent'},{text:'Kal-El'},{text:'Bruce Wayne'},{text:'Jor-El'}],
+    infoUrl:'https://www.google.com/?ion=1&espv=2#q=superman%27s%20origin',
+    submitter:'Quick Polls'
+  }
+  ,{
+    question: 'Which of the below has never portrayed Selena Kyle (AKA Catwoman) in a screen adaptation?',
+    choices:[{text:'Anne Hathaway'},{text:'Michelle Pfeiffer'},{text:'Halle Berry'},{text:'Camren Bicondova'}],
+    submitter: 'Quick Polls'
+  }
+  ,{
+    question: 'What is The Flash, Barry Allen’s, occupation?',
+    choices:[{text:'Forensic Scientist'},{text:'Aerospace Engineer'},{text:'Journalist'},{text:'Test Pilot'}],
+    submitter: 'Quick Polls'
+  }
+  ,{
+    question: 'Dick Grayson, former protégé of Bruce Wayne was once his side-kick Robin. What is his new super hero alias?',
+    choices:[{text:'Batboy'},{text:'Nightwing'},{text:'Midnighter'},{text:'Darkwing'}],
+    submitter: 'Quick Polls'
+  }
+  ,{
+    question: 'Wonder Woman, the Amazon Princess shares her real name with another famous princess. What is it?',
+    choices:[{text:'Catherine'},{text:'Beatrice'},{text:'Margaret'},{text:'Diana'}],
+    submitter: 'Quick Polls'
+  }
+  ,{
+    question: 'Which English city is John Constantine originally from?',
+    choices:[{text:'Liverpool'},{text:'Manchester'},{text:'Peterborough'},{text:'Bristol'}],
+    submitter: 'Quick Polls'
+  }
+  ,{
+    question: 'Alfred is Bruce Wayne, AKA Batman’s butler. What is his surname?',
+    choices:[{text:'Cogsworth'},{text:'Pennyworth'},{text:'Moneypenney'},{text:'Jeeves'}],
+    submitter: 'Quick Polls'
+  }
+  ,{
+    question: 'What was Harley Quinn’s former occupation?',
+    choices:[{text:'Comedian'},{text:'Research Assistant'},{text:'Nurse'},{text:'Psychiatrist'}],
+    submitter: 'Quick Polls'
+  }
+  ,{
+    question: 'Who was Clarke Kent’s first love interest?',
+    choices:[{text:'Lois Lane'},{text:'Lana Lang'},{text:'Lisa Lopez'},{text:'Lorraine Lesley'}],
+    submitter: 'Quick Polls'
+  }
+  ,{
+    question: 'What is not one of Bruce Wayne’s aliases?',
+    choices:[{text:'Dark knight'},{text:'Batman'},{text:'Darkwing'},{text:'Matches Malone'}],
+    submitter: 'Quick Polls'
+  }
+]
+}
